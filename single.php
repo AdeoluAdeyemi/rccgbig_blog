47089f3c6d6fa268d7658f3f01085a2d0a66bc96
Adeolu Adeyemi <98715389+AdeoluAdeyemi@users.noreply.github.com>
2024-09-15 05:47:59 +0100

    WIP - Header banner

--- single.php
+++ single.php
@@ -12,7 +12,46 @@ get_header();
 
 	<main id="primary" class="site-main">
 		<section class="page-section bg-gradient-gray-light-1 bg-scroll overflow-hidden">
+			<div class="bg-shape-1 wow fadeIn no-animate animated">
+				<img alt="" loading="lazy" width="1443" height="844" decoding="async" data-nimg="1" class="opacity-05" src="/assets/images/banner/bg-shape-1.svg" style="color: transparent;">
+			</div>
 
+			<div class="container position-relative pt-10 pt-sm-40 text-center">
+				<div class="row">
+					<div class="col-lg-10 offset-lg-1">
+						<h1 class="hs-title-9 mb-20">
+							<?php the_title();?>
+            </h1>
+						<div class="blog-item-data mt-30 mt-sm-10 mb-0 wow fadeIn no-animate animated">
+
+						</div>
+						<div class="blog-item-data mt-30 mt-sm-10 mb-0 wow fadeIn no-animate  animated" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s;">
+							<div class="d-inline-block me-3">
+								<a href="#">
+									<i class="mi-clock size-16"></i>
+									<span class="visually-hidden">Date:</span> <?php echo $date->format('Y-m'); ?>
+								</a>
+							</div>
+							<div class="d-inline-block me-3">
+								< href="#">
+									<i class="mi-user size-16"></i>
+									<span class="visually-hidden">Author:</span> <?php echo $author ?>
+								</a>
+							</div>
+							<div class="d-inline-block me-3">
+								<i class="mi-folder size-16"></i>
+								<span class="visually-hidden">Categories:</span><a href="#">Design</a>, 
+								<a href="#">Branding</a>
+							</div>
+							<div class="d-inline-block me-3">
+								<a href="#">
+									<i class="mi-message size-16"></i> 5 Comments
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</section>
 
 		<section class="page-section">
