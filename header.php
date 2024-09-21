d7c78d10a3f781f7ece6bc20d52f8766389cc6d0
Adeolu Adeyemi <98715389+AdeoluAdeyemi@users.noreply.github.com>
2024-09-21 23:01:47 +0100

    WIP - Fixing header

--- header.php
+++ header.php
@@ -27,24 +27,22 @@
 		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'rccgbig_blog' ); ?></a>
 
 		<header id="masthead" class="site-header">
-			<div class="site-branding">
-				<?php
-				the_custom_logo();
-				if ( is_front_page() && is_home() ) :
-					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php
-				else :
-					?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php
-				endif;
-				$rccgbig_blog_description = get_bloginfo( 'description', 'display' );
-				if ( $rccgbig_blog_description || is_customize_preview() ) :
-					?>
-					<p class="site-description"><?php echo $rccgbig_blog_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-				<?php endif; ?>
-			</div><!-- .site-branding -->
+			<div class="container pt-2 pb-2">
+				<div class="row">
+					<div class="col site-header__logo">
+						<?php the_custom_logo(); ?>
+					</div>
+					<div class="col-md-5">
+						search
+					</div>
+				</div>
+			</div>
+
+
+
+
+
+
 
 			<nav id="site-navigation" class="main-nav stick-fixed wow-menubar wch-unset js-transparent fadeInDown animated transparent">
 				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'rccgbig_blog' ); ?></button>
